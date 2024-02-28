e4266eb2a986f6c9438395384da7c81213367691
Nathan <nathan.bechon@free.fr>
2024-02-28 13:35:55 +0100

    test curser

--- script.js
+++ script.js
@@ -29,8 +29,18 @@ window.onload = () => {
     });
 });
 
-var cursor = document.getElementById("cursor");
-document.body.addEventListener("mousemove", function(e) {
-  cursor.style.left = e.clientX + "px",
-    cursor.style.top = e.clientY + "px";
-});
+import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+
+neonCursor({
+  el: document.getElementById('site'),
+  shaderPoints: 16,
+  curvePoints: 80,
+  curveLerp: 0.5,
+  radius1: 5,
+  radius2: 30,
+  velocityTreshold: 10,
+  sleepRadiusX: 100,
+  sleepRadiusY: 100,
+  sleepTimeCoefX: 0.0025,
+  sleepTimeCoefY: 0.0025
+})
