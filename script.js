f241f57554d31498c515a20d414c6fd78bf9ae5a
Nathan <nathan.bechon@free.fr>
2024-03-05 14:46:28 +0100

    sd

--- script.js
+++ script.js
@@ -9,13 +9,4 @@ window.onload = () => {
         document.getElementById("scrollbarre").style.width = barre + "px";
     });
  };
-
- const words = document.querySelectorAll('.word');
-
- words.forEach((word, index) => {
-   const delay = parseFloat(word.style.getPropertyValue('--delay')) * 1000; // Convertit le délai en millisecondes
-   setTimeout(() => {
-     word.style.transform = 'translateY(100%)'; // Déplace le mot vers le bas
-   }, delay);
- });
  
