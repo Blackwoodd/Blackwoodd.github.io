9b4439dd6e8ce05bbc7c633e79df71fcc02f6d80
Nathan <nathan.bechon@free.fr>
2024-03-06 23:52:34 +0100

    sd

--- script.js
+++ script.js
@@ -10,16 +10,3 @@ window.onload = () => {
     });
  };
  
- window.addEventListener('scroll', function() {
-    var target = document.getElementById('target');
-    var targetPosition = target.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight / 2;
-
-    if (targetPosition < screenPosition) {
-        target.style.opacity = '1';
-        target.classList.add('animate'); // Ajoutez une classe pour déclencher l'animation CSS
-    } else {
-        target.style.opacity = '0';
-        target.classList.remove('animate'); // Supprimez la classe pour arrêter l'animation CSS
-    }
-});
